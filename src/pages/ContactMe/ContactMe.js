7d48c73d1d17c698d96db3deda65f97aa2791084
Franklin samuvel <franklinsamuvel955@gmail.com>
2024-10-31 22:02:39 +0530

    contact page responsiveness

--- src/pages/ContactMe/ContactMe.js
+++ src/pages/ContactMe/ContactMe.js
@@ -5,10 +5,10 @@ import Form from "../../components/Form/Form";
 const ContactMe = () => {
   return (
     <div id="contactMe" className="contact-me-container">
-      <h2 className="header-contact">contact ME</h2>
+      <h2 className="header-contact">Contact Me</h2>
       <p className="intro-text">
-        Here you will find more information contact me, what I do, and my current
-        skills mostly in terms of programming and technology.
+        Here you will find more information to contact me, what I do, and my
+        current skills mostly in programming and technology.
       </p>
       <div className="contact-content">
         <Form />
